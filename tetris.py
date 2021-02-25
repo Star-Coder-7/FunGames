2c21db5ce2a723e5e8d6f23e79d9fcad0f3858a5
Shiven Singhal <19singsh@hitchinboys.co.uk>
2021-02-25 18:58:34 +0000

    initial commit

--- tetris.py
+++ tetris.py
@@ -205,4 +205,54 @@ def drawTextMiddle(text, size, color, surface):
 
 
 def drawGrid(surface, row, col):
+    sx = topLeft_x
+    sy = topLeft_y
+    for i in range(row):
+        pygame.draw.line(surface, (128, 128, 128), (sx, sy + i * 30), (sx + playWidth, sy + i * 30))    # horizontal
+        for j in range(col):
+            pygame.draw.line(surface, (128, 128, 128), (sx + j * 30, sy), (sx + j * 30, sy + playHeight))   # vertical
+
+
+def clearRows(grid, locked):
+    # need to see if row is clear the shift every other row above down one
+
+    inc = 0
+    for i in range(len(grid) - 1, - 1, - 1):
+        row = grid[i]
+        if (0, 0, 0) not in row:
+            inc += 1
+            # add positions to remove from locked
+            ind = i
+            for j in range(len(row)):
+                try:
+                    del locked[(j, i)]
+                except:
+                    continue
+
+    if inc > 0:
+        for key in sorted(list(locked), key=lambda x: x[1])[::-1]:
+            x, y = key
+            if y < ind:
+                newKey = (x, y + inc)
+                locked[newKey] = locked.pop(key)
+
+
+def drawNextShape(shape, surface):
+    font = pygame.font.SysFont('comicsans', 30)
+    label = font.render('Next Shape', 1, (255, 255, 255))
+
+    sx = topLeft_x + playWidth + 50
+    sy = topLeft_y + playHeight / 2 - 100
+    format = shape.shape[shape.rotation % len(shape.shape)]
+
+    for i, line in enumerate(format):
+        row = list(line)
+        for j, column in enumerate(row):
+            if column == '0':
+                pygame.draw.rect(surface, shape.color, (sx + j * 30, sy + i * 30, 30, 30), 0)
+
+    surface.blit(label, (sx + 10, sy - 30))
+
+
+def drawWindow(surface):
     pass
